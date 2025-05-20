0097f88b500ed0ce22e91909c4835c509069e298
subbu <subbunandyala48@gmail.com>
2025-05-20 18:58:45 +0530

    hello world

--- roboshop.sh
+++ roboshop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-AMI_ID="ami-09c813fb71547fc4f"
+AMI_ID="ami_09c813fb71547fcc4f"
 SG_ID="sg-04f5b8fdb267df1bd" # Replace with your SG ID
 INSTANCES=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z06528725AJCPEPL0K7K"
 DOMAIN_NAME="subbuaws.site"
 
-for instace in ${INSTANCES[@]}
+for instance in ${INSTANCES[@]}
 do
 
   INSTANCE_ID=$(aws ec2 run-instances \ --image-id ami-09c813fb71547fc4f \ --instance-type t2.micro \
