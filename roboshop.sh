431cac48fc658e1e08e1d02b08293dbca3d99bcb
subbu <subbunandyala48@gmail.com>
2025-05-20 19:31:58 +0530

    hello world

--- roboshop.sh
+++ roboshop.sh
@@ -9,10 +9,14 @@ DOMAIN_NAME="subbuaws.site"
 for instance in ${INSTANCES[@]}
 do
 
-  INSTANCE_ID=$(aws ec2 run-instances \ --image-id ami-09c813fb71547fc4f \ --instance-type t2.micro \
-   --security-group-ids sg-04f5b8fdb267df1bd \ --tag-specifications 'ResourceType=instance,
-   Tags=[{Key=Name,Value=test}]' --query "Instances[0].PrivateIpAddress" --output text)
-               
+  INSTANCE_ID=$$(aws ec2 run-instances \
+  --image-id "$ami-09c813fb71547fc4f" \
+  --instance-type t2.micro \
+  --security-group-ids "$sg-04f5b8fdb267df1bd" \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+  --query "Instances[0].InstanceId" \
+  --output text)
+  
     if [ $instance != "frontend" ]
     then 
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
