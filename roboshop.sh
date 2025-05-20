decfcc9bc409c6d24fda022040ee2bd45415288b
subbu <subbunandyala48@gmail.com>
2025-05-20 19:19:50 +0530

    hello world

--- roboshop.sh
+++ roboshop.sh
@@ -10,15 +10,14 @@ for instance in ${INSTANCES[@]}
 do
 
   INSTANCE_ID=$(aws ec2 run-instances \ --image-id ami-09c813fb71547fc4f \ --instance-type t2.micro \
-  --security-group-ids sg-04f5b8fdb267df1bd \ --tag-specifications 'ResourceType=instance,
+   --security-group-ids sg-04f5b8fdb267df1bd \ --tag-specifications 'ResourceType=instance,
    Tags=[{Key=Name,Value=test}]' --query "Instances[0].PrivateIpAddress" --output text)
-    
                
     if [ $instance != "frontend" ]
     then 
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-    fi
+    fi 
      echo "$instance IP address: $IP"
 done
