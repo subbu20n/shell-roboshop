eafc9a48e8a379b67a60bd84882b776ff3c492e7
subbu <subbunandyala48@gmail.com>
2025-05-21 18:15:52 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -78,7 +78,7 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongod.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb client"
 
-STATUS=$(mongosh --host mongodb.subbuaws.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")'
+STATUS=$(mongosh --host mongodb.subbuaws.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -ne 0 ]
 then
     mongosh --host mongodb.subbuaws.site </app/db/master-data.js &>>$LOG_FILE
