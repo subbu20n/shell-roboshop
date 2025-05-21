0b6ffd544a78a8b2beba31e2cfc4b5e839c5475c
subbu <subbunandyala48@gmail.com>
2025-05-21 17:50:38 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -33,55 +33,55 @@ else
 fi    
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? " Disabling nodejs -y"
 
-dnf module enabling nodejs:20 -y
+dnf module enabling nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling nodejs:20 -y"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs:20"
 
 id roboshop 
 if [ $? -ne 0 ]
 then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating system user roboshop"
 else 
     echo -e "system user roboshop already created ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOG_FILE
 VALIDATE $? "creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
 VALIDATE $? "Downloading catalogue"
 
 rm -rf /app/*
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "unzipping catalogue"
 
 cd /app
-npm install
+npm install &>>$LOG_FILE
 VALIDATE $? "Installing dependencies"
 
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Copying catalogue service"
 
-systemctl daemon-reload
-systemctl enable catalogue
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
 systemctl start catalogue
 VALIDATE $? "Starting catalogue"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongod.repo
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb client"
 
 STATUS=$(mongosh --host mongodb.subbuaws.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")'
 if [ $STATUS -ne 0 ]
 then
-    mongosh --host mongodb.subbuaws.site </app/db/master-data.js
+    mongosh --host mongodb.subbuaws.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into mongodb"
 else
     echo -e "data is already loaded ... $Y SKIPPING $N"
