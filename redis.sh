000110b61578131c7625b481b65dd88e8feed9dc
subbu <subbunandyala48@gmail.com>
2025-05-21 19:35:20 +0530

    hello world started

--- redis.sh
+++ redis.sh
@@ -43,8 +43,8 @@ VALIDATE $? "Enabling Redis:7"
 dnf install redis -y &>>LOG_FILE
 VALIDATE $? "Installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
-VALIDATE $? "Edited redis.conf to access remote connections"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
 
 systemctl enable redis &>>LOG_FILE
 VALIDATE $? "Enabling redis"
