428a8f2444744100d834aa3108a572a5a0966523
subbu <subbunandyala48@gmail.com>
2025-05-21 23:08:29 +0530

    hello world started

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
+
 START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo "script started execution at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check the user have root priviliges or not
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-   echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
-   exit 1 # give other than 0 upto 127
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-   echo "you are running with root access"  | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
-echo "please enter root password to setup"
-read -s $RABBITMQ_PASSWD
 
-#validate function takes input as exit status what command they tried to install
+echo "Please enter rabbitmq password to setup"
+read -s RABBITMQ_PASSWD
+
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-if [ $1 -eq 0 ]
-then
-    echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-else
-    echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-fi
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-VALIDATE $? "Adding rabbitm repo"
+VALIDATE $? "Adding rabbitmq repo"
 
 dnf install rabbitmq-server -y &>>$LOG_FILE
 VALIDATE $? "Installing rabbitmq server"
@@ -51,6 +53,6 @@ rabbitmqctl add_user roboshop $RABBITMQ_PASSWD &>>$LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
