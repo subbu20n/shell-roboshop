983373cf0bbb574d4c600e22d1303cbeec61c985
subbu <subbunandyala48@gmail.com>
2025-05-21 23:59:43 +0530

    hello world started

--- payment.sh
+++ payment.sh
@@ -73,6 +73,6 @@ systemctl start payment
 VALIDATE $? "starting payment"
 
 END_TIME=$(date +%s)
-TOTAL-TIME=$(($END_TIME - $START_TIME))
+TOTAL_TIME=$(($END_TIME - $START_TIME))
 
 echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
