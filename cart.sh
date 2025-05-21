dc43b71f685928bb2a687570169e6d1d90046b15
subbu <subbunandyala48@gmail.com>
2025-05-21 20:40:28 +0530

    hello world started

---
+++ cart.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started execution at: $(date)" | tee -a $LOG_FILE
+
+# check the user have root priviliges or not
+if [ $USERID -ne 0 ]
+then
+   echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
+   exit 1 # give other than 0 upto 127
+else
+   echo "you are running with root access"  | tee -a $LOG_FILE
+fi
+
+#validate function takes input as exit status what command they tried to install
+VALIDATE(){
+if [ $1 -eq 0 ]
+then
+    echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+else
+    echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+fi
+}
+
+dnf module  disable nodejs -y
+VALIDATE $? "Disabling nodejs -y"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling nodejs:20 -y"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs -y"
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating system user roboshop"
+else
+    echo -e "system user roboshop already created ...$Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zi
+VALIDATE $? "Downloading cart"
+
+rm -rf /app/*
+cd /app
+unzip /tmp/cart.zip
+VALIDATE $? "Unzipping cart"
+
+cd /app
+npm install
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Copying cart service"
+
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
+VALIDATE $? "Starting cart"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
+
+
