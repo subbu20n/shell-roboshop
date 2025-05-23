d187e6dcce1eb0f909c32fa7ac84f85a6cad1e6a
subbu <subbunandyala48@gmail.com>
2025-05-23 15:43:42 +0530

    hello world started

--- mongodb.sh
+++ mongodb.sh
@@ -1,60 +1,58 @@
 #!/bin/bash
 
-
-START_TIME=$(date +%S)
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-LOGS_FOLDER="/VAR/LOG/roboshop-logs"
+LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo "Script started execution at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# chech the user have root privileges or not
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 # give other than 0 upto 127
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-    echo "you are running with root access" | tee -a $LOG_FILE
-fi 
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
 
-# validate function takes input as exit status what command they tried to install
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-    echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-else
-    echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-    exit 1
-fi        
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongod.repo
-VALIDATE $? "Copying mongodb repo"
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "Copying MongoDB repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing mongodb"
+VALIDATE $? "Installing mongodb server"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enabling mongodb"
+VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "Starting mongodb"
+VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "Editing mongod.conf for remote connections"
+VALIDATE $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "Restarting mongodb"
+VALIDATE $? "Restarting MongoDB"
 
-END_TIME=$(date +%S)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
