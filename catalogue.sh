8cac7a71d5530a2cbc416501c1e3d53d841bec97
subbu <subbunandyala48@gmail.com>
2025-05-23 14:29:49 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -90,7 +90,7 @@ else
 fi
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 
 echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
