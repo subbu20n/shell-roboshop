bac83c934bc2d03a44f04e453e83ec0f6110450d
subbu <subbunandyala48@gmail.com>
2025-05-23 12:12:56 +0530

    hello world started

---
+++ frontend.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+START_TIME=$(date +%S)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/VAR/LOG/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started execution at: $(date)" | tee -a $LOG_FILE
+
+# chech the user have root privileges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 # give other than 0 upto 127
+else
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi 
+
+# validate function takes input as exit status what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+    echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+else
+    echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+fi        
+}
+
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "Disabling nginx"
+
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nginx"
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping frontend"
+
+rm -rf /usr/share/nginx/html &>>$LOG_FILE
+VALIDATE $? "Removing default nginx.conf"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarting nginx"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
