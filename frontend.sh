a82cd8609ede85996712df3d87e961ff23a24c3c
subbu <subbunandyala48@gmail.com>
2025-05-23 14:37:34 +0530

    hello world started

--- frontend.sh
+++ frontend.sh
@@ -33,7 +33,7 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nginx -y &>>$LOG_FIL
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Nginx"
 
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
