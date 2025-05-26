f0245723f72a8b4164131d549281a230f9a8be98
subbu <subbunandyala48@gmail.com>
2025-05-26 11:30:13 +0530

    hello world started

--- user-set.sh
+++ user-set.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -e
+set -e 
 
 failure(){
-    echo "Failed at : $1 $2"
+    echo "failed at: $1 $2"
 }
 
 trap 'failure "${LINENO}" "${BASH_COMMAND}"' ERR
-
 
 START_TIME=$(date +%s)
 USERID=$(id -u)
