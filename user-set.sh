3ac9a5ef0368edc3360245c6abbfb33234ff57bb
subbu <subbunandyala48@gmail.com>
2025-05-26 11:24:59 +0530

    hello world started

--- user-set.sh
+++ user-set.sh
@@ -29,16 +29,7 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+
 
 dnf module disable nodejs -y &>>$LOG_FILE
 
