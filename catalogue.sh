ae0b92bbf0ce618ed2002d311d9c9eb320e6ccb8
subbu <subbunandyala48@gmail.com>
2025-06-01 11:52:06 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -57,6 +57,7 @@ VALIDATE $? "Creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
 VALIDATE $? "downloading catalogue"
 
+rm -rf /app/*
 cd /app 
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "Unzipping catalogue"
